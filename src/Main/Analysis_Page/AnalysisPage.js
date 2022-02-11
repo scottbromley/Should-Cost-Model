ab93f6b0054b3a8a3545aa87d653c2414d3e1adc
Scott Bromley <scottbromley17@gmail.com>
2022-02-11 07:49:44 +0000

    animation push

--- src/Main/Analysis_Page/AnalysisPage.js
+++ src/Main/Analysis_Page/AnalysisPage.js
@@ -1,24 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
 
 function AnalysisPage() {
-    return (
-        <div
+  const [divToggle, setDivToggle] = useState(false);
+
+  const list = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren",
+        staggerChildren: 0.3,
+      },
+    },
+  }
+  
+  const item = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  }
+  
+
+  return (
+    <div
+      style={{
+        with: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <buton
         style={{
-          color: "grey",
-          width: "100%",
-          height: "100%",
-          display: "flex",
+          border: "1px solid white",
+          backgroundColor: "orange",
+          color: "white",
+          padding: "10px",
+          borderRadius: "5px",
+          justifyConent: "center",
           alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column"
         }}
+        onClick={() => setDivToggle(!divToggle)}
       >
-        <h1>Coming Soon!</h1>
-        <p>This is the space where further analysis of the models can be conducted. The output of all of the models can be computed and information about which recipes have the most potential for cost saving can be viewed here.</p>
-        <p>The entire database of recipes and ingredients can be viewed in one place and compared against each other.</p>
-        <p>There can also be functionality to aggregate by category: grouped by supplier, material type, lead buyer etc..</p>
-      </div>
-    )
+        Click me!
+      </buton>
+      <br />
+      
+
+      <AnimatePresence>
+        {divToggle && (
+          <motion.ul initial="hidden" animate="visible" variants={list}>
+            <motion.li variants={item} >Item 1</motion.li>
+            <motion.li variants={item} >Item 2</motion.li>
+            <motion.li variants={item} >Item 3</motion.li>
+            <motion.li variants={item} >Item 4</motion.li>
+            <motion.li variants={item} >Item 5</motion.li>
+          </motion.ul>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default AnalysisPage
+export default AnalysisPage;
+
+// <div
+// style={{
+//   color: "grey",
+//   width: "100%",
+//   height: "100%",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+//   flexDirection: "column"
+// }}
+// >
+// <h1>Coming Soon!</h1>
+// <p>This is the space where further analysis of the models can be conducted. The output of all of the models can be computed and information about which recipes have the most potential for cost saving can be viewed here.</p>
+// <p>The entire database of recipes and ingredients can be viewed in one place and compared against each other.</p>
+// <p>There can also be functionality to aggregate by category: grouped by supplier, material type, lead buyer etc..</p>
+// </div>
--- src/Main/AppSidebar.js
+++ src/Main/AppSidebar.js
@@ -1,37 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AppSidebar.css';
 import logo from './should-cost-logo-v2.png';
 import { VscSettingsGear } from "react-icons/vsc";
 import { SiReact } from "react-icons/si";
 import { GoGraph } from "react-icons/go";
 import { BsGraphDown } from "react-icons/bs";
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
 
 
 function AppSidebar({selectPage, selectedPage}) {
+
+    const [ sidebarLoadToggle, setSidebarLoadToggle ] = useState(false);
+
+    useEffect(()=>{
+        setSidebarLoadToggle(true);
+    }, [])
+
+    const list = {
+        visible: {
+          opacity: 1,
+          transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.3,
+          },
+        },
+        hidden: {
+          opacity: 0,
+          transition: {
+            when: "afterChildren",
+            staggerChildren: 0.3,
+          },
+        },
+      }
+      
+      const item = {
+        visible: { opacity: 1, x: 0 },
+        hidden: { opacity: 0, x: -100 },
+      }
 
     return (
         <div className='app__sidebar__outer'>
             <div className='app__sidebar__logo'>
                 <img src={logo} alt="Greencore Logo" width="60" height="60"/>
             </div>
-            <div className='app__sidebar__content'>
-                <div className={`app__sidebar__button__outer ${selectedPage === "SETUP" && 'app__sidebar__active'}`} onClick={()=>selectPage("SETUP")}>
-                    <VscSettingsGear size="30" className={selectedPage === "SETUP" && 'app__sidebar__rotate'}/>
-                    <p>Setup</p>
-                </div>
-                <div className={`app__sidebar__button__outer ${selectedPage === "MODEL" && 'app__sidebar__active'}`} onClick={()=>selectPage("MODEL")}>
-                    <SiReact size="30" className={selectedPage === "MODEL" && 'app__sidebar__rotate'}/>
-                    <p>Modelling</p>
-                </div>
-                <div className={`app__sidebar__button__outer ${selectedPage === "ANALYSIS" && 'app__sidebar__active'}`} onClick={()=>selectPage("ANALYSIS")}>
-                    <GoGraph size="30"/>
-                    <p>Analysis</p>
-                </div>
-                <div className={`app__sidebar__button__outer ${selectedPage === "TRENDING" && 'app__sidebar__active'}`} onClick={()=>selectPage("TRENDING")}>
-                    <BsGraphDown size="30"/>
-                    <p>Trending</p>
-                </div>
-            </div> 
+            <AnimatePresence>
+                {sidebarLoadToggle && (
+                    <div className='app__sidebar__content'>
+                        <motion.ul  initial="hidden"
+                                    animate="visible"
+                                    variants={list}
+                                    style={{listStyleType: "none", padding: "0px", margin: "0px"}}
+                                    >
+                            <motion.li variants={item}>
+                                <div key="1" className={`app__sidebar__button__outer ${selectedPage === "SETUP" && 'app__sidebar__active'}`} onClick={()=>selectPage("SETUP")}>
+                                    <VscSettingsGear size="30" className={selectedPage === "SETUP" && 'app__sidebar__rotate'}/>
+                                    <p>Setup</p>
+                                </div>
+                            </motion.li>
+                            <motion.li variants={item}>
+                                <div key="2" className={`app__sidebar__button__outer ${selectedPage === "MODEL" && 'app__sidebar__active'}`} onClick={()=>selectPage("MODEL")}>
+                                    <SiReact size="30" className={selectedPage === "MODEL" && 'app__sidebar__rotate'}/>
+                                    <p>Modelling</p>
+                                </div>
+                            </motion.li>
+                            <motion.li variants={item}>
+                                <div key="3" className={`app__sidebar__button__outer ${selectedPage === "ANALYSIS" && 'app__sidebar__active'}`} onClick={()=>selectPage("ANALYSIS")}>
+                                    <GoGraph size="30"/>
+                                    <p>Analysis</p>
+                                </div>
+                            </motion.li>
+                            <motion.li variants={item}>
+                                <div key="4" className={`app__sidebar__button__outer ${selectedPage === "TRENDING" && 'app__sidebar__active'}`} onClick={()=>selectPage("TRENDING")}>
+                                    <BsGraphDown size="30"/>
+                                    <p>Trending</p>
+                                </div>
+                            </motion.li>
+                        </motion.ul> 
+                    </div>
+                )}
+            </AnimatePresence>
         </div>
     )
 }
